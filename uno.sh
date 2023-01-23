a22bdbbc0c2284a06ba14e1a459e7390de7dd00e
mamoro02 <mamoro02@floridauniversitaria.es>
2023-01-23 10:42:40 +0100

    primer ejerciocio del grupo 1

---
+++ uno.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo "Escribe tu nombre"
+read nombre
+
+echo "Escribe tu primer apellido"
+read apellido1
+
+echo "Escribe tu segunda apellido"
+read apellido2
+
+sleep 5
+
+echo "Tu nombre completo es $name $apellido1 $apellido"
