c458e7f98d40fbb34bef0b50047674a38f64957b
mamoro02 <mamoro02@floridauniversitaria.es>
2023-01-26 10:35:59 +0100

    actualización

---
+++ grupo II/seis.sh
@@ -0,0 +1,9 @@
+#1/bin/bash
+
+echo "**************************"
+echo "****MENÚ******************"
+echo "****1) Buscar título******"
+echo "****2) Buscar año*********"
+echo "****3) Buscar aditorial***"
+echo "****4) Buscar género******"
+echo "****5) Buscar título******"
