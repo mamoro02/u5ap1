2a0eabe094e2e9bc2633dd2f469bc403ad8a9672
mamoro02 <mamoro02@floridauniversitaria.com>
2023-01-26 17:44:26 +0100

    ej 6 grup 1

---
+++ grupo I/seis.sh
@@ -0,0 +1,30 @@
+#1/bin/bash
+
+echo "**************************"
+echo "****MENÚ******************"
+echo "****1) Buscar título******"
+echo "****2) Buscar año*********"
+echo "****3) Buscar aditorial***"
+echo "****4) Buscar género******"
+echo "****5) Buscar título******"
+
+read -p "Dame una opción: " opcion
+
+while
+case $opcion in
+    1)
+    read -p "Dame un título: " titulo
+    cat bdlibros.txt | grep "$titulo"
+    ;;
+    2)
+    read -p "Dame un año: "anyo
+    cat bdlibros.txt | grep "$anyo"
+    ;;
+    3)
+    read -p "Dame un editorial: " editorial
+    cat bdlibros.txt | grep "$editorial"
+    ;;
+    4)
+    read -p "Dame un genero: " genero
+    cat bdlibros.txt | grep "$genero"
+    ;;
